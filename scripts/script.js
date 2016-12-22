ac8646aa4eec222bd3504185d375e2718b9d6b26
elrihaz <elrihaz@gmail.com>
2016-12-22 14:14:21 +0100

    ajout bannieres

--- scripts/script.js
+++ scripts/script.js
@@ -9,25 +9,55 @@ $(document).ready(function () {
     $('ul li').removeClass("active"); // - active à tous
     $(this).addClass("active"); // + active à celle cliquée
   });
-  // chargement du contenu html du thème dans main
+  // chargement du contenu html dans main et de la banniere du thème choisi
   // accueil
   $('#SK').click(function () {
-    $("#main").load("html/accueil.html");
+    $("#main").load("html/accueil.html"); // contenu
+    // - toutes les ban
+    $('#banniere').removeClass("ban-bienvenue");
+    $('#banniere').removeClass("ban-croque");
+    $('#banniere').removeClass("ban-collabo");
+    $('#banniere').removeClass("ban-audio");
+    $('#banniere').addClass("ban-accueil"); // + ban-accueil
   });
   // bienvenue la chance
   $('#bienvenue').click(function () {
     $("#main").load("html/bienvenuelachance.html");
+    // - toutes les ban
+    $('#banniere').removeClass("ban-accueil");
+    $('#banniere').removeClass("ban-croque");
+    $('#banniere').removeClass("ban-collabo");
+    $('#banniere').removeClass("ban-audio");
+    $('#banniere').addClass("ban-bienvenue"); // + ban-bienvenue
   });
   // croque-meurtre
   $('#croque').click(function () {
     $("#main").load("html/croque-meurtre.html");
+    // - toutes les ban
+    $('#banniere').removeClass("ban-accueil");
+    $('#banniere').removeClass("ban-bienvenue");
+    $('#banniere').removeClass("ban-collabo");
+    $('#banniere').removeClass("ban-audio");
+    $('#banniere').addClass("ban-croque"); // + ban-croque
   });
   // collaborations
   $('#collabo').click(function () {
     $("#main").load("html/collaborations.html");
+    // - toutes les ban
+    $('#banniere').removeClass("ban-accueil");
+    $('#banniere').removeClass("ban-bienvenue");
+    $('#banniere').removeClass("ban-croque");
+    $('#banniere').removeClass("ban-audio");
+    $('#banniere').addClass("ban-collabo"); // + ban-collabo
   });
   // audio-connaissances
   $('#audio').click(function () {
     $("#main").load("html/audioconnaissances.html");
+    // - toutes les ban
+    $('#banniere').removeClass("ban-accueil");
+    $('#banniere').removeClass("ban-bienvenue");
+    $('#banniere').removeClass("ban-croque");
+    $('#banniere').removeClass("ban-collabo");
+    $('#banniere').addClass("ban-audio"); // + ban-audio
   });
 });
