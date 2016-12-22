cce89fc033d683f555b48c97020d47f1fd9ceddd
elrihaz <elrihaz@gmail.com>
2016-12-22 15:31:28 +0100

    fond variable

--- scripts/script.js
+++ scripts/script.js
@@ -4,60 +4,74 @@ $(document).ready(function () {
   $("#main").load("html/accueil.html");
 
   // Clic sur le menu
-  // Mise à jour du theme actif ds le menu
+
+  // Mise à jour du theme actif ds le menu et - ban et fonds
   $('ul li').click(function () {
     $('ul li').removeClass("active"); // - active à tous
     $(this).addClass("active"); // + active à celle cliquée
-  });
-  // chargement du contenu html dans main et de la banniere du thème choisi
-  // accueil
-  $('#SK').click(function () {
-    $("#main").load("html/accueil.html"); // contenu
     // - toutes les ban
+    $('#banniere').removeClass("ban-accueil");
     $('#banniere').removeClass("ban-bienvenue");
     $('#banniere').removeClass("ban-croque");
     $('#banniere').removeClass("ban-collabo");
     $('#banniere').removeClass("ban-audio");
-    $('#banniere').addClass("ban-accueil"); // + ban-accueil
+    // - tous les fonds
+    $('#contenu').removeClass("fond-noir");
+    $('#contenu').removeClass("fond-blanc");
+    $('#contenu').removeClass("fond-gris");
   });
+
+
+  // chargement du contenu html dans main et de la banniere du thème choisi
+
+  // accueil
+  $("#SK").click(function () {
+    // contenu
+    $("#main").load("html/accueil.html");
+    // + ban-accueil
+    $('#banniere').addClass("ban-accueil");
+    // + fond blanc
+    $('#contenu').addClass("fond-blanc");
+
+  });
+
   // bienvenue la chance
   $('#bienvenue').click(function () {
+    // contenu
     $("#main").load("html/bienvenuelachance.html");
-    // - toutes les ban
-    $('#banniere').removeClass("ban-accueil");
-    $('#banniere').removeClass("ban-croque");
-    $('#banniere').removeClass("ban-collabo");
-    $('#banniere').removeClass("ban-audio");
-    $('#banniere').addClass("ban-bienvenue"); // + ban-bienvenue
+    // + ban-bienvenue
+    $('#banniere').addClass("ban-bienvenue");
+    // + fond noir
+    $('#contenu').addClass("fond-noir");
   });
+
   // croque-meurtre
   $('#croque').click(function () {
+    // contenu
     $("#main").load("html/croque-meurtre.html");
-    // - toutes les ban
-    $('#banniere').removeClass("ban-accueil");
-    $('#banniere').removeClass("ban-bienvenue");
-    $('#banniere').removeClass("ban-collabo");
-    $('#banniere').removeClass("ban-audio");
-    $('#banniere').addClass("ban-croque"); // + ban-croque
+    // + ban-croque
+    $('#banniere').addClass("ban-croque");
+    // + fond gris
+    $('#contenu').addClass("fond-gris");
   });
+
   // collaborations
   $('#collabo').click(function () {
+    // contenu
     $("#main").load("html/collaborations.html");
-    // - toutes les ban
-    $('#banniere').removeClass("ban-accueil");
-    $('#banniere').removeClass("ban-bienvenue");
-    $('#banniere').removeClass("ban-croque");
-    $('#banniere').removeClass("ban-audio");
-    $('#banniere').addClass("ban-collabo"); // + ban-collabo
+    // + ban-collabo
+    $('#banniere').addClass("ban-collabo");
+    // + fond blanc
+    $('#contenu').addClass("fond-blanc");
   });
+
   // audio-connaissances
   $('#audio').click(function () {
+    // contenu
     $("#main").load("html/audioconnaissances.html");
-    // - toutes les ban
-    $('#banniere').removeClass("ban-accueil");
-    $('#banniere').removeClass("ban-bienvenue");
-    $('#banniere').removeClass("ban-croque");
-    $('#banniere').removeClass("ban-collabo");
-    $('#banniere').addClass("ban-audio"); // + ban-audio
+    // + ban-audio
+    $('#banniere').addClass("ban-audio");
+    // + fond blanc
+    $('#contenu').addClass("fond-blanc");
   });
 });
