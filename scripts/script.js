9d294dcc2b229af4c1764f47a399a8054a4eecf5
elrihaz <elrihaz@gmail.com>
2016-12-23 15:16:36 +0100

    Mod collabo et bouton retour haut page

--- scripts/script.js
+++ scripts/script.js
@@ -7,8 +7,12 @@ $(document).ready(function () {
 
   // Mise à jour du theme actif ds le menu et - ban et fonds
   $('ul li').click(function () {
-    $('ul li').removeClass("active"); // - active à tous
-    $(this).addClass("active"); // + active à celle cliquée
+    // - active à tous
+    $('ul li').removeClass("active");
+    // + active à celle cliquée
+    $(this).addClass("active");
+    // retour haut de page
+    $('html, body').animate({scrollTop: 0}, 0);
     // - toutes les ban
     $('#banniere').removeClass("ban-accueil");
     $('#banniere').removeClass("ban-bienvenue");
@@ -74,4 +78,25 @@ $(document).ready(function () {
     // + fond blanc
     $('#contenu').addClass("fond-blanc");
   });
+
+  // bouton retour haut page
+  var duration = 500;
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      // Si un défillement de 100 pixels ou plus.
+      // Ajoute le bouton
+      $('.cRetour').fadeIn(duration);
+    } else {
+      // Sinon enlève le bouton
+      $('.cRetour').fadeOut(duration);
+    }
+  });
+
+  $('.cRetour').click(function(event) {
+    // Un clic provoque le retour en haut animé.
+    event.preventDefault();
+    $('html, body').animate({scrollTop: 0}, duration);
+    return false;
+  })
+
 });
